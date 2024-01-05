0676916b1d025faa9f2a64679215ae3c31702f29
PRUTHVI R <PRUTHVI R@Administrator>
2024-01-05 12:20:39 +0530

    mvn project

---
+++ zohoCRM1/src/test/java/com/CRM/testscript/CustomerModule.java
@@ -0,0 +1,13 @@
+package com.CRM.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CustomerModule {
+	@Test
+	public void createcustomer() {
+		Reporter.log("create customer",true);
+	}
+	
+
+}
